4d093f86626fd691244e1fd828f2b2ae87359beb
obaytimur <baytimurogulcan@gmail.com>
2022-06-02 21:22:03 +0300

    SRTF, input from txt, and IO part added

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 // ready 0
 // running 1
 // empty 2
+// IO 3
 #define stackSize 4096 // Given in homework parameter
 
 struct ThreadInfo threadArray[6]; // Array of threadInfo objects initialized
@@ -13,11 +14,22 @@ bool isFinished = false;            // A bool variable to flag the end of the ex
 // Moreover, runThread must be written before initializeThread since initialization requires runThread function to use
 // in makecontext().
 void exitThread(int threadIndex) {  // Function for thread to exit
-    free(threadArray[threadIndex].context.uc_stack.ss_sp);  // It, firstly, frees the memory of the object
     threadArray[threadIndex].state = -1;    // Then it sets the state of the object to finished
 }
 
 void runThread(int threadIndex){    // Function that runs the threads
+    bool isIoFound=false;
+    for(int i=1; i<6; i++){
+        if (threadArray[i].state == 3){
+            printf("Running>T%d \t \t \t \t \t \t \t \t IO>T%d\n",threadArray[threadIndex].threadNumber,threadArray[i].threadNumber);
+            isIoFound = true;
+            exitThread(i);
+            break;
+        }
+    }
+    if(!isIoFound) {
+        printf("Running>T%d \t \t \t \t \t \t \t \t IO>\n", threadArray[threadIndex].threadNumber);
+    }
     threadArray[threadIndex].state = 1;     // Starts by setting flag to running
     threadArray[threadIndex].countNumber += 1;  // Increase counter one as one execution is done
 
@@ -25,14 +37,14 @@ void runThread(int threadIndex){    // Function that runs the threads
         printf("\t");   // Prints enough tabs for the corresponding thread
     }
     // Prints the counter value of the thread
-    printf("%d, %d \n", threadArray[threadIndex].countNumber, threadArray[threadIndex].exeNumber);
+    printf("%d \n", threadArray[threadIndex].countNumber);
     sleep(1); // Thread sleeps for one second to represent the operation time
     threadArray[threadIndex].countNumber += 1; // The same operations are done for the second time,
 
     for(int i=0; i<threadIndex - 1; i++){
         printf("\t");
     }
-    printf("%d, %d \n", threadArray[threadIndex].countNumber, threadArray[threadIndex].exeNumber);
+    printf("%d \n", threadArray[threadIndex].countNumber);
     threadArray[threadIndex].state = 0; // At the end of the second execution, state is set to ready again since runs are done
 
     // Here I assumed, every execution value which will be given by the user was going to be even number as it
@@ -40,7 +52,7 @@ void runThread(int threadIndex){    // Function that runs the threads
     // for it to be the next even number, e.g. 6, then exits the thread. This if statement can also be put
     // above and made for the odd ones too.
     if(threadArray[threadIndex].countNumber >= threadArray[threadIndex].exeNumber){
-        exitThread(threadIndex);
+        threadArray[threadIndex].state = 3;
     }
     sleep(1); // Again sleeps for one second representing execution
 }
@@ -99,22 +111,58 @@ void scheduler_lottery(int initial){
     }
 }
 
-int main(/* int argc, char **argv */){      // main takes input values for the threads
+void scheduler_SRTF(int initial){
+    signal(SIGALRM,scheduler_SRTF);
+    alarm(2);
+    int shortestRemExeInd = 0;
+    int exeValue = 999;
+    for(int i=1; i<6; i++){
+        if(threadArray[i].exeNumber < exeValue && threadArray[i].state != -1 && threadArray[i].state != 3){
+            exeValue = threadArray[i].exeNumber;
+            shortestRemExeInd = i;
+        }
+    }
+    if(shortestRemExeInd == 0){
+        printf("All threads are finished! \n");
+        exit(-1);
+    }
+    swapcontext(&threadArray[0].context, &threadArray[shortestRemExeInd].context);   // swapcontext calls the function which
+    // is defined in initialization for the thread, this part runs the runThread
+    if (isAllFinished() ) {      // Then it is checked whether all the threads are done or not
+        isFinished = true;
+    }
+}
+
+int main(){      // main takes input values for the threads from the txt file
+    FILE *fileObject ;
+    fileObject= fopen("/home/ogi/Media/ODTÜ EE/4-2/442/HW2/input.txt","r");
+    int exeNumberArray[7];
+    if(fileObject==NULL) {
+        printf("ERROR: Could not open file: input.txt");
+        return 1;
+    }
+    while(!feof(fileObject)) {
+        for (int i = 0; i < 7; i++) {
+            fscanf(fileObject, "%d", &exeNumberArray[i]);
+        }
+    }
+    fclose(fileObject);
+
     srand(time(NULL));      // srand function is needed for the lottery selection, if not used lottery selection
     // were following the same sequence
-    int exeNumberArray[6] = {0,2,4,8,6,4};  // Arrays hold the execution values
-//    exeNumberArray[0] = 0 ;
-    int total = 20;
-    /*
-    for(int i=1; i<6; i++){
-        exeNumberArray[i] = atoi(argv[i]);
-        total = total + exeNumberArray[i];
-    }
-     */
+
+    int total = 0;
     int threadIndex;
     for(int i=0; i<6; i++){     // threads are created and initialized by calling the functions
         createThread(i);
         initializeThread( exeNumberArray[i], i);
+        total += exeNumberArray[i];
+    }
+    if(exeNumberArray[6] == 0){
+        printf("Scheduler is selected as Lottery. \n");
+    }
+    else if (exeNumberArray[6] == 1){
+        printf("Scheduler is selected as SRTF. \n");
     }
     // From here to getcontext line, main trivially prints writings
     printf("Share: \n");
@@ -124,20 +172,20 @@ int main(/* int argc, char **argv */){      // main takes input values for the t
             printf("\n");
         }
     }
-    for(int i=0; i<6; i++){
-        printf("%d \t %d \t %d \t %d\n",threadArray[i].state, threadArray[i].countNumber, threadArray[i].exeNumber, threadArray[i].threadNumber);
-    }
     sleep(1);
     printf("Threads:\n T1\tT2\tT3\tT4\tT5\n");
 
     sleep(1);
     getcontext(&threadArray[0].context);    // Main thread's context is called to be used in the functions
 
-    scheduler_lottery(0);   // scheduler is called, since it is self calling function, until stopped it will run indefinitely
-                                  // with the times of the alarm
+    if(exeNumberArray[6] == 0){
+        scheduler_lottery(0);   // scheduler is called, since it is self calling function, until stopped it will run indefinitely
+        // with the times of the alarm
+    }
+    else if (exeNumberArray[6] == 1){
+        scheduler_SRTF(0);
+    }
     while(!isFinished){
     }
-    printf("All threads are finished! \n");
     return 0;
-
 }
